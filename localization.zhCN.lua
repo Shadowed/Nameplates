b42a06d33c64e37fa60bc065276b414c1497eb00
Shadowed <shadowed.wow@gmail.com>
2009-04-24 16:40:44 +0000

    * Added zhCN and zhTW (Thanks Sparanoid)

---
+++ localization.zhCN.lua
@@ -0,0 +1,64 @@
+if( GetLocale() ~= "zhCN" ) then
+	return
+end
+
+NameplatesLocals = {
+	-- Status
+	["Enemy player/npc name plates are now visible."] = "敌对玩家/NPC姓名板当前为显示状态",
+	["Enemy player/npc name plates are now hidden."] = "敌对玩家/NPC姓名板当前为隐藏状态",
+	["Friendly player/npc name plates are now visible."] = "好友玩家/NPC姓名板当前为显示状态",
+	["Friendly player/npc name plates are now hidden."] = "好友玩家/NPC姓名板当前为隐藏状态",
+	["All name plates are now visible."] = "所有姓名板当前为显示状态",
+	["All name plates are now hidden."] = "所有姓名板当前为隐藏状态",
+
+	-- GUI 
+	["General"] = "常规设置",
+	["Bars"] = "Bars",
+	["Bar texture"] = "姓名板纹理",
+	["Shadow"] = "阴影",
+
+	["Custom texture to use for health and cast bars in name plates."] = "自定义姓名板与施法条的材质",
+
+	["Name text"] = "姓名文字设置",
+	["Cast/Health text"] = "施法条/血量设置",
+	["Level text"] = "等级文字设置",
+	["Text"] = "字体设置",
+	["Font name"] = "字体名称",
+
+	["Font size"] = "字体大小",
+	["Font border"] = "字体描边",
+
+	["Health text display"] = "血量文字显示",
+	["Cast text display"] = "施法文字显示",
+	["Style of display for health bar text."] = "更改血量文字显示的样式",
+	["Style of display for cast bar text."] = "更改施法文字显示的样式",
+
+	["Outline"] = "标准描边",
+	["Thick outline"] = "厚描边",
+	["Monochrome"] = "单线",
+
+	["Font"] = "字体设置",
+	["Nameplates"] = "姓名板",
+	["Hide elite indicator"] = "隐藏精英指示器",
+
+	["Min / Max"] = "最小值/最大值",
+	["Deficit"] = "损失血量",
+	["Percent"] = "百分比",
+	["None"] = "无",
+	["Current / Max"] = "当前血量/最大值",
+	["Current"] = "当前血量",
+	["Percent"] = "百分比",
+	["Time left"] = "剩余时间",
+
+	["Enable shadow"] = "开启阴影",
+	["Shadow color"] = "阴影颜色",
+	["Shadow offset X"] = "阴影 X 偏移",
+	["Shadow offset Y"] = "阴影 Y 偏移",
+
+	["Show nameplate visibility status"] = "提示当前姓名板显示状态",
+	["Hide health bar border"] = "隐藏血条边框",
+	["Hide cast bar border"] = "隐藏施法条边框",
+
+	["A UI reload is required to make the border show again."] = "只有当你重载界面后才能再次显示边框",
+	["A UI reload is required to make the elite indicator show again."] = "只有当你重载界面后才能再次显示精英指示器",
+}
---
+++ localization.zhTW.lua
@@ -0,0 +1,64 @@
+if( GetLocale() ~= "zhTW" ) then
+	return
+end
+
+NameplatesLocals = {
+	-- Status
+	["Enemy player/npc name plates are now visible."] = "敵對玩家/NPC姓名板當前為顯示狀態",
+	["Enemy player/npc name plates are now hidden."] = "敵對玩家/NPC姓名板當前為隱藏狀態",
+	["Friendly player/npc name plates are now visible."] = "好友玩家/NPC姓名板當前為顯示狀態",
+	["Friendly player/npc name plates are now hidden."] = "好友玩家/NPC姓名板當前為隱藏狀態",
+	["All name plates are now visible."] = "所有姓名板當前為顯示狀態",
+	["All name plates are now hidden."] = "所有姓名板當前為隱藏狀態",
+	
+	-- GUI
+	["General"] = "常規設置",
+	["Bars"] = "Bars",
+	["Bar texture"] = "姓名板紋理",
+	["Shadow"] = "陰影",
+	
+	["Custom texture to use for health and cast bars in name plates."] = "自定義姓名板與施法條的材質",
+	
+	["Name text"] = "姓名文字設置",
+	["Cast/Health text"] = "施法條/血量設置",
+	["Level text"] = "等級文字設置",
+	["Text"] = "字體設置",
+	["Font name"] = "字體名稱",
+
+	["Font size"] = "字體大小",
+	["Font border"] = "字體描邊",
+
+	["Health text display"] = "血量文字顯示",
+	["Cast text display"] = "施法文字顯示",
+	["Style of display for health bar text."] = "更改血量文字顯示的樣式",
+	["Style of display for cast bar text."] = "更改施法文字顯示的樣式",
+
+	["Outline"] = "標準描邊",
+	["Thick outline"] = "厚描邊",
+	["Monochrome"] = "單線",
+	
+	["Font"] = "字體設置",
+	["Nameplates"] = "姓名板",
+	["Hide elite indicator"] = "隱藏精英指示器",
+
+	["Min / Max"] = "最小值/最大值",
+	["Deficit"] = "損失血量",
+	["Percent"] = "百分比",
+	["None"] = "無",
+	["Current / Max"] = "當前血量/最大值",
+	["Current"] = "當前血量",
+	["Percent"] = "百分比",
+	["Time left"] = "剩餘時間",
+
+	["Enable shadow"] = "開啟陰影",
+	["Shadow color"] = "陰影顏色",
+	["Shadow offset X"] = "陰影 X 偏移",
+	["Shadow offset Y"] = "陰影 Y 偏移",
+	
+	["Show nameplate visibility status"] = "提示當前姓名板顯示狀態",
+	["Hide health bar border"] = "隱藏血條邊框",
+	["Hide cast bar border"] = "隱藏施法條邊框",
+	
+	["A UI reload is required to make the border show again."] = "只有當你重載界面后才能再次顯示邊框",
+	["A UI reload is required to make the elite indicator show again."] = "只有當你重載界面后才能再次顯示精英指示器",
+}
