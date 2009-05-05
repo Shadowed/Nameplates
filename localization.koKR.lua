1b4b1f80a5b2d766e4ce3adc5a2d5dfb321a63be
Shadowed <shadowed.wow@gmail.com>
2009-05-05 14:45:12 +0000

    * Added koKR localization (Thanks mrgyver)

---
+++ localization.koKR.lua
@@ -0,0 +1,64 @@
+if( GetLocale() ~= "koKR" ) then
+	return
+end
+
+NameplatesLocals = {
+  -- Status
+	["Enemy player/npc name plates are now visible."] = "적대적 Player/NPC 이름표 표시.",
+	["Enemy player/npc name plates are now hidden."] = "적대적 Player/NPC 이름표 숨김.",
+	["Friendly player/npc name plates are now visible."] = "우호적 Player/NPC 이름표 표시.",
+	["Friendly player/npc name plates are now hidden."] = "우호적 Player/NPC 이름표 숨김.",
+	["All name plates are now visible."] = "모든 이름표 표시.",
+	["All name plates are now hidden."] = "모든 이름표 숨김.",
+
+	-- GUI 
+	["General"] = "일반",
+	["Bars"] = "Bars",
+	["Bar texture"] = "무늬 선택",
+	["Shadow"] = "그림자",
+
+	["Custom texture to use for health and cast bars in name plates."] = "이름표 체력바와 시전바에 사용자 정의 글꼴 사용.",
+
+	["Name text"] = "이름",
+	["Cast/Health text"] = "시전/체력",
+	["Level text"] = "레벨",
+	["Text"] = "글꼴",
+	["Font name"] = "글꼴 선택",
+
+	["Font size"] = "글자 크기",
+	["Font border"] = "글자 테두리",
+
+	["Health text display"] = "체력바 표시 설정",
+	["Cast text display"] = "시전바 표시 설정",
+	["Style of display for health bar text."] = "체력바 글꼴 표시 스타일 설정.",
+	["Style of display for cast bar text."] = "시전바 글꼴 표시 스타일 설정.",
+
+	["Outline"] = "외곽선",
+	["Thick outline"] = "굵은 외곽선",
+	["Monochrome"] = "단색",
+
+	["Font"] = "글꼴",
+	["Nameplates"] = "이름표",
+	["Hide elite indicator"] = "정예 표시 숨김.",
+
+	["Min / Max"] = "최소 / 최대",
+	["Deficit"] = "결손치",
+	["Percent"] = "백분율",
+	["None"] = "없음",
+	["Current / Max"] = "현재값 / 최대값",
+	["Current"] = "현재값",
+	["Percent"] = "백분율",
+	["Time left"] = "남은 시간",
+
+	["Enable shadow"] = "그림자 사용",
+	["Shadow color"] = "그림자 색",
+	["Shadow offset X"] = "그림자 X축",
+	["Shadow offset Y"] = "그림자 Y축",
+
+	["Show nameplate visibility status"] = "가시거리에 따른 이름표 보기",
+	["Hide health bar border"] = "체력바 테두리 숨김",
+	["Hide cast bar border"] = "시전바 테두리 숨김",
+
+	["A UI reload is required to make the border show again."] = "선택한 테두리 옵션을 보려면 재시작이 필요합니다.",
+	["A UI reload is required to make the elite indicator show again."] = "선택한 정예 표시 옵션을 보려면 재시작이 필요합니다.",
+}
